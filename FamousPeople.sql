1e4068425871d1bcb072d7e1e42a4fc73a8cea50
MoKi <61506669+moki929@users.noreply.github.com>
2020-09-20 23:16:20 -0400

    added computer scientists

---
+++ FamousPeople.sql
@@ -0,0 +1,98 @@
+-- small set of “famous people
+-- another table for things they do
+
+CREATE TABLE computer_scientists
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name TEXT
+);
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (1, "Panini");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (2, "Al-Khwarizmi");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (3, "Banu Musa");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (4, "Al-Jazari");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (5, "Llull, Ramon");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (6, "Pascal, Blaise");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (92, "Pearl, Judea");
+
+INSERT INTO computer_scientists
+  (id, name)
+VALUES
+  (93, "LeCun, Yann");
+
+CREATE TABLE achievements
+(
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  Achievement TEXT,
+  year INTEGER
+);
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (1, -500, "Ashtadhyayi Sanskrit grammar was systematised and technical, using metarules, transformations, and recursions, a forerunner to formal language theory and basis for Panini-Backus form used to describe programming languages");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (2, 830, "algorithm is derived from the algorism, the technique of performing arithmetic with Hindu–Arabic numerals popularised by al-Khwarizmi in his book On the Calculation with Hindu Numerals");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (3, 850, "wrote the Book of Ingenious Devices, where they described what appears to be the first programmable machine, an automatic flute player");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (4, 1206, "Invented programmable machines, including programmable humanoid robots, and the castle clock, an astronomical clock considered the first programmable analog computer");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (5, 1300, "Designed multiple symbolic representations machines, and pioneered notions of symbolic representation and manipulation to produce knowledge—both of which were major influences on Leibniz");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (6, 1642, "Invented the mechanical calculator");
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (92, 2011, "Fundamental contributions to artificial intelligence through the development of a calculus for probabilistic and causal reasoning"
+);
+
+INSERT INTO achievements
+  (id, Achievement, year)
+VALUES
+  (93, 2018, "optical character recognition and computer vision using convolutional neural networks (CNN), and is a founding father of convolutional nets");
+
+  
