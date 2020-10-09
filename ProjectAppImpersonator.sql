5757a8c43b869b07952e215d836a5b378a723d85
MoKi <61506669+moki929@users.noreply.github.com>
2020-10-09 17:47:03 -0400

    set up for end

---
+++ ProjectAppImpersonator.sql
@@ -0,0 +1,9 @@
+a game that stores scores
+the app stores your data in a SQL database 
+write SQL statements that might look like their own SQL.
+
+CREATE a table to store the data.
+INSERT a few example rows in the table.
+Use an UPDATE to emulate what happens when you edit data in the app.
+Use a DELETE to emulate what happens when you delete data in the app.
+
